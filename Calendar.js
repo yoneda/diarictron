78efb44469467e63296b5f8e8f3ea42c7252f749
yoneda <nsshowdream@gmail.com>
2020-12-21 11:28:05 +0900

    [fix] add table

--- Calendar.js
+++ Calendar.js
@@ -9,12 +9,83 @@ const Wrapper = styled.div`
   box-sizing: border-box;
 `;
 
+const Row = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+`;
+
+const Cell = styled.div`
+  width: 20px;
+  height: 20px;
+  border: solid 1px black;
+  box-sizing: border-box;
+`;
+
 function Calendar() {
   return (
     <Wrapper>
       <h2>calendar</h2>
-      <div>header</div>
-      <div>table</div>
+      <div>
+        2020年12月
+        <button>&lt;</button>
+        <button>&gt;</button>
+      </div>
+      <div>
+        <Row>
+          <div>月</div>
+          <div>火</div>
+          <div>水</div>
+          <div>木</div>
+          <div>金</div>
+          <div>土</div>
+          <div>日</div>
+        </Row>
+        <Row>
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+        </Row>
+        <Row>
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+        </Row>
+        <Row>
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+        </Row>
+        <Row>
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+        </Row>
+        <Row>
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+        </Row>
+      </div>
     </Wrapper>
   );
 }
