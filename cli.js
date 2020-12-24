cb7e0af04d78f7d863c1c9f9a19fcaf40736e4f8
yoneda <nsshowdream@gmail.com>
2020-12-25 01:43:49 +0900

    [fix] cli.js use process.argv

--- cli.js
+++ cli.js
@@ -6,14 +6,28 @@ const faker = require("faker");
 const { nanoid } = require("nanoid");
 const dayjs = require("dayjs");
 
-db.defaults({ notes: [], user: {} }).write();
+function main() {
+  db.defaults({ notes: [], user: {} }).write();
+  db.set("user", { showCal: false, dark: false, start: "saturday" }).write();
+  db.update("notes", () =>
+    [...Array(3)].map(() => ({
+      id: nanoid(8),
+      body: faker.lorem.word(),
+      createdAt: dayjs().format("YYYY-MM-DDTHH:mm:ss[Z]"),
+    }))
+  ).write();
+}
 
-db.set("user", { showCal: false, dark: false, start: "saturday" }).write();
+function year() {
+  console.log("year");
+}
 
-db.update("notes", () =>
-  [...Array(3)].map(() => ({
-    id: nanoid(8),
-    body: faker.lorem.word(),
-    createdAt: dayjs().format("YYYY-MM-DDTHH:mm:ss[Z]"),
-  }))
-).write();
+// process.argv is an array containing the command line arguments.
+// The first element will be 'node', the second element will be the name of the JavaScript file.
+// The next elements will be any additional command line arguments.
+const arg = process.argv.slice(2)[0];
+if (arg === "year") {
+  year();
+} else {
+  main();
+}
