d0013d29856742db23709afe0579988d50759b38
yoneda <nsshowdream@gmail.com>
2021-01-29 13:41:43 +0900

    [fix] remove unused import

--- NoteList.js
+++ NoteList.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { useStoreState, useStoreActions } from "easy-peasy";
-import { appendFile } from "fs";
 
 const Wrapper = styled.div`
   border: solid 1px black;
