0dadc1263f166dd641077e63cc6a7b64d7c02bd4
yoneda <nsshowdream@gmail.com>
2021-02-23 11:07:01 +0900

    [clean] refactor chip

--- Chip.js
+++ Chip.js
@@ -14,26 +14,13 @@ const Wrapper = styled.div`
   ${({ type }) => {
     if (type === "outlined") {
       return css`
-        border: 1px solid dodgerblue;
+        border: 1px solid #dedede;
         background-color: white;
-        color: dodgerblue;
-        &:hover {
-          background-color: rgba(30, 144, 255, 0.05);
-        }
-      `;
-    } else if (type === "contained") {
-      return css`
-        border: none;
-        background-color: dodgerblue;
-        color: white;
-        box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%),
-          0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);
-        &:hover {
-          background-color: rgb(21, 111, 198);
-        }
       `;
     } else {
       return css`
+        border: none;
+        background-color: #dedede;
       `;
     }
   }}
@@ -44,7 +31,9 @@ function Chip(props) {
   return (
     <Wrapper type={type} onClick={() => onClick()}>
       {children}
-      <div onClick={()=>onClose()}><Cancel /></div>
+      <div onClick={() => onClose()}>
+        <Cancel />
+      </div>
     </Wrapper>
   );
 }
