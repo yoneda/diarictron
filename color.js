a0fd682052a74ad942f0fdae1d432c2f7767ac10
yoneda <nsshowdream@gmail.com>
2021-03-01 20:12:26 +0900

    [design] add color const

---
+++ color.js
@@ -0,0 +1,30 @@
+export const LIGHT_BLUE_50 = "#E1F5FE";
+export const LIGHT_BLUE_100 = "#B3E5FC";
+export const LIGHT_BLUE_200 = "#81D4FA";
+export const LIGHT_BLUE_300 = "#4FC3F7";
+export const LIGHT_BLUE_400 = "#29B6F6";
+export const LIGHT_BLUE_500 = "#03A9F4";
+export const LIGHT_BLUE_600 = "#039BE5";
+export const LIGHT_BLUE_700 = "#0288D1";
+export const LIGHT_BLUE_800 = "#0277BD";
+export const LIGHT_BLUE_900 = "#01579B";
+
+export const INDIGO_50 = "#E8EAF6";
+export const INDIGO_100 = "#C5CAE9";
+export const INDIGO_200 = "#9FA8DA";
+export const INDIGO_300 = "#7986CB";
+export const INDIGO_400 = "#5C6BC0";
+export const INDIGO_500 = "#3F51B5";
+export const INDIGO_600 = "#3949AB";
+export const INDIGO_700 = "#303F9F";
+export const INDIGO_800 = "#283593";
+export const INDIGO_900 = "#1A237E";
+
+export const RED_500 = "#F44336";
+export const RED_600 = "#E53935";
+export const RED_700 = "#D32F2F";
+export const RED_800 = "#C62828";
+export const RED_900 = "#B71C1C";
+
+export const BLACK = "#000000";
+export const WHITE = "#FFFFFF";
