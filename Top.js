7fc505b2dfacddb07186d1f3069adf2ca8f09a3c
yoneda <nsshowdream@gmail.com>
2021-03-24 12:12:56 +0900

    [fix] add some contents to the top component

--- Top.js
+++ Top.js
@@ -1,11 +1,22 @@
 import React, { Fragment, useEffect } from "react";
 import { Router, Link, navigate } from "@reach/router";
+import Button from "./Button";
+import Acunit from "./Acunit";
 
 function Top() {
   return (
     <div>
-      <h2>This is top page</h2>
-      <button onClick={() => navigate("/app")}>DEMO</button>
+      <h2><Acunit />Diarictron</h2>
+      <p>毎日を記録しよう✋</p>
+      <ul>
+        <li>✓ダークモード</li>
+        <li>✓キーボードショートカット</li>
+        <li>✓マークダウン対応</li>
+        <li>✓オープンソース</li>
+      </ul>
+      <Button type="outlined" onClick={() => navigate("/app")}>VIEW DEMO</Button>
+      <Button type="outlined" onClick={() => navigate("/app")}>DOWNLOAD</Button>
+      <footer>ソースコード | 利用規約 | プライバシーポリシー</footer>
     </div>
   );
 }
